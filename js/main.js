a7ef55231e0e0654f9acdb86a32ce6b7aeb04ada
Ahmed Gamal <ahmgy.mail@gmail.com>
2023-10-14 22:40:52 +0300

    coloring the board

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 "use strict";
 
-let board = document.querySelector(".board");
 let color = "black";
+let board = document.querySelector(".board");
+let select = document.querySelector(".btn-invisible");
 
 // * Creates a grid of squares on the board element.
 function createGrid(size) {
@@ -12,9 +13,11 @@ function createGrid(size) {
   for (let i = 0; i < size * size; i++) {
     let square = document.createElement("div");
     square.classList.add("square");
+    square.addEventListener("mouseover", colorSquare);
     board.insertAdjacentElement("beforeend", square);
   }
 }
+
 // Create grid after pager load
 createGrid(16);
 
@@ -30,3 +33,53 @@ function changeSize(input) {
       "display:flex;justify-content: center;font-size:18px;";
   }
 }
+
+// Sets the value of the 'mouseClicked' variable to true when the mouse button is pressed down on the 'board' element, and sets it to false when the mouse button is released.
+let mouseClicked = false;
+
+board.addEventListener("mousedown", () => {
+  mouseClicked = true;
+});
+
+board.addEventListener("mouseup", () => {
+  mouseClicked = false;
+});
+
+// Changes the color of a square on the board based on the current color and mode selected.
+function colorSquare() {
+  const mode = document.querySelector(".mode");
+
+  if (mouseClicked) {
+    mode.textContent = "Mode: Coloring";
+
+    switch (color) {
+      case "random":
+        this.style.backgroundColor = `hsl(${Math.random() * 360},100%,50%)`;
+        break;
+      case "select":
+        this.style.backgroundColor = select.value;
+        break;
+      case "white":
+        mode.textContent = "Mode: NOT Coloring";
+        this.style.backgroundColor = color;
+        break;
+      default:
+        this.style.backgroundColor = color;
+        break;
+    }
+  } else {
+    mode.textContent = "Mode: NOT Coloring";
+  }
+}
+
+// This function assigns the value of `choice` to the `color` variable.
+function changeColor(choice) {
+  color = choice;
+}
+
+// When the button is clicked, it triggers a click event on another element with the class name "btn-invisible".
+document
+  .querySelector("button.opener")
+  .addEventListener("click", () =>
+    document.querySelector(".btn-invisible").click()
+  );
